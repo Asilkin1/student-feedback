b6423c2408b5d1049e63188aa55c7c72ab09f25d
Alexander Silkin <mrmajestickk@gmail.com>
2021-01-22 10:45:47 -0500

    attempting to pass ccode and scode to the feedback form

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
 # @login_manager.unauthorized_handler
 @app.route('/signup',methods=['GET'])
 def unauthorized():
-      if 'student' in request.url_rule.rule:
+      if request.path == '/student':
           return render_template('student_sign_up.html', title='instructor')
       else:
           return render_template('professor_sign_up.html')
@@ -50,13 +50,29 @@ def registration():
     if password == repassword:
         return redirect(url_for('index'))
 
+# New student signup
+@app.route('/newstudent', methods=['GET'])
+def newstudent():
+    #class code
+    classCode = request.form.get('classCode')
+    
+    #student code
+    studentCode = request.form.get('studentCode')
+
+    return render_template('student_sign_up.html', scode=12003)
+
+
+    
+
 @app.route('/student', methods=["POST", "GET"])
 # @login_required
 def student():
     if request.method == 'GET':
         #Delete existing data in database (can change this later)
         #delete_posts()
-        pass
+        ccode = request.args.get('classCode')
+        return render_template('student.html', classcode=ccode)
+        
 
     if request.method == 'POST':
         #Date
