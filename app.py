14a1a4defb717aa84ed716235129e98f130633df
Alexander Silkin <mrmajestickk@gmail.com>
2021-01-25 14:26:55 -0500

    Sync my app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Flask barebones
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, redirect, url_for, make_response, session, abort, flash
 from models import create_post, get_posts, delete_posts, create_class, get_class, delete_class
 from datetime import date, datetime
 import time
@@ -12,12 +12,96 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import io
 
+# Login/Logout/Registration
+from sqlalchemy.orm import sessionmaker
+from CreateUserDatabase import *
+import os
+
+# Login engine
+engine = create_engine('sqlite:///tutorial.db', echo=True)
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=["POST", "GET"])
 def index():
+    # Go to student page, no need to be registered
+    if request.path == '/student':
+          return render_template('student_sign_up.html', title='student_page')
+    
     return render_template('index.html', title='submit')
+
+# -------------------------------------------------------------------- Log in
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        send_username = str(request.form['username'])
+        send_password = str(request.form['password'])
+
+        # If the password and username is provided
+        if send_password and send_username:
+            Session = sessionmaker(bind=engine)
+            s = Session()
+
+            query = s.query(User).filter(User.username.in_([send_username]), User.password.in_([send_password]))
+            result = query.first()
+
+            if result:
+                session['logged_in'] = True
+                session['studentCode'] = "2479YH"
+                session['username'] = send_username
+                flash('Your are successfully logged in','success')
+
+                return render_template('index.html')
+            
+            else:
+                flash('Wrong password or username','error')
+
+        else:
+            flash('Check your username and password','error')
+            session['logged_in'] = False 
+
+    return render_template('login.html')
+
+# --------------------------------------------------------------------- Log out
+@app.route('/logout')
+def logout():
+    # User is logged out
+    session['logged_in'] = False
+    # User is removed from the session
+    session.pop('username', None)
+    session.pop('studentCode', None)
+    # Feedback message
+    flash('You are successfully logged out','success')
+    return render_template('login.html')
+
+# --------------------------------------------------------------------- Registration
+@app.route('/register', methods=['GET','POST'])
+def register():
+    flash('Please register to gain access to the website','info')
+    if request.method == 'POST':
+        
+        # Get username from the form
+        username = request.form.get('username')
+        password = request.form.get('password')
+        repassword = request.form.get('repassword')
+
+        if username and password and repassword:
+            # Passwords should match
+            if str(password) == str(repassword):
+                Session = sessionmaker(bind=engine)
+                s = Session()
+                user = User(username,password)
+                s.add(user)
+                s.commit()
+                flash('You are registered successfully','success')
+            else:
+                flash("Password doesn't match",'error')
+        else:
+            flash('Fields cannot be empty','error')
+
+    return render_template('register.html')
+
 
 @app.route('/signup', methods=["POST", "GET"])
 def signup():
@@ -52,6 +136,9 @@ def student():
 
         # create data in database
         create_post(dateNow, timeNow, classCode, studentCode, emoji, elaborateNumber, elaborateText)
+        
+        # Message
+        flash('Thank you for your feedback','info')
 
     return render_template('student.html', title='student')
 
@@ -60,10 +147,16 @@ def student():
 #The "Dashboard" of the professor. Right now shows any classes that they create along with its class code and has a button to create a new class
 @app.route('/professor', methods=["POST", "GET"])
 def submission():
-    con = sl.connect('prof.db')
-    c = con.cursor()
-    result = c.execute("SELECT * FROM account").fetchall()
-    con.close()
+    if not session.get('logged_in'):
+            flash('Please log in to gain access to the website','info')
+            return render_template('login.html', title='submit')
+    else: 
+        category = request.args.get('category')
+        return render_template('professor.html', title='instructor')
+        con = sl.connect('prof.db')
+        c = con.cursor()
+        result = c.execute("SELECT * FROM account").fetchall()
+        con.close()
 
     return render_template('dashboard.html', title='dashboard', data = result)
 
@@ -225,4 +318,5 @@ def calc(classCode,Category):
     return f'Your average score was {round(Frame.mean(),2)}' #return the mean
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
     app.run(debug=True)
