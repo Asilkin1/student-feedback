f0c346f7a4eaf3caa57fb5936f3d4e469a498ca1
Alexander Silkin <mrmajestickk@gmail.com>
2021-02-04 16:24:23 -0500

    new database

--- CreateUserDatabase.py
+++ CreateUserDatabase.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
-engine = create_engine('sqlite:///united.db', echo=True,connect_args={"check_same_thread": False})
+engine = create_engine('sqlite:///united_1.db', echo=True,connect_args={"check_same_thread": False})
 Base = declarative_base()
 
 # User database(professor)
